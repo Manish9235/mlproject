aa4042a84408c9f37933a741c5a7e3891c1336ce
Manish Dongre <118501933+Manish9235@users.noreply.github.com>
2024-08-07 15:07:26 +0530

    Data Transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -80,15 +80,15 @@ class DataTransformation:
       input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
       target_feature_test_df=test_df[target_column_name]
 
-      logging.info("Applying preprocessing object on training dataframe and testing dataframe.")
+      logging.info(f"Applying preprocessing object on training dataframe and testing dataframe.")
 
-      input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-      input_feature_test_arr=preprocessing_obj.fit_transform(input_feature_test_df)
+      input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+      input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
       train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
       test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
-      logging.info("Saved preprocessing object.")
+      logging.info(f"Saved preprocessing object.")
 
       save_object(
         file_path = self.data_transformation_config.preprocessor_obj_file_path,
